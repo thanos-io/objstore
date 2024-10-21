56060d9b82f82e5801d93f7de19b81a0ef69e515
milinddethe15 <milinddethe15@gmail.com>
2024-10-21 22:39:01 +0530

    CHANGELOG and TODO
    Signed-off-by: milinddethe15 <milinddethe15@gmail.com>

--- providers/bos/bos.go
+++ providers/bos/bos.go
@@ -66,7 +66,7 @@ func parseConfig(conf []byte) (Config, error) {
 
 // NewBucket new bos bucket.
 func NewBucket(logger log.Logger, conf []byte, component string) (*Bucket, error) {
-	// TODO(https://github.com/thanos-io/objstore/pull/140): Add support for custom roundtripper.
+	// TODO(https://github.com/thanos-io/objstore/pull/150): Add support for roundtripper wrapper.
 	if logger == nil {
 		logger = log.NewNopLogger()
 	}
